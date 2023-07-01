256f95d921b9c66b2fd7df3df3b522a8a8f2fbdb
[David Sanchez] <[sanchezdavid1440@gmail.com]>
2023-06-30 20:46:27 -0500

    docs: v1.0.0

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -81,13 +81,13 @@
      // turno de la computadora
     const turnoComputadora = ( puntosMinimos ) => {
         let puntosComputadora = 0
-         do {
-             const carta = pedirCarta( deck);
-             puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1 );
-             crearCarta(carta, puntosJugadores.length - 1 )
-         } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) )
-         determinarGanador();
-     }
+        do {
+            const carta = pedirCarta( deck);
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1 );
+            crearCarta(carta, puntosJugadores.length - 1 )
+        } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) )
+        determinarGanador();
+    }
 
 
 
